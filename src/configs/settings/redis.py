222165bbb13e617c170ca0d61a463468a1bd8079
tarunshankarguhacogoport <Tarun.Guha@cogoport.com>
2024-08-20 13:57:15 +0530

    ENV

--- src/configs/settings/redis.py
+++ src/configs/settings/redis.py
@@ -6,16 +6,16 @@ class RedisSettings(BaseSettings):
     redis_port: str = Field(..., env="REDIS_PORT")
     redis_username: str = Field(..., env="REDIS_USERNAME")
     redis_password: str = Field(..., env="REDIS_PASSWORD")
-    redis_database: int = Field(0, env="REDIS_DATABASE")
+    redis_database: int = Field(0, env="REDIS_DATABASE")  # Default to 0 if not set
 
     class Config:
         env_file = ".env"
 
     def get_redis_url(self) -> str:
-        return "rediss://{}:{}@{}:{}/{}".format(
-            self.redis_username,
-            self.redis_password,
-            self.redis_host,
-            self.redis_port,
-            self.redis_database,
+        return "rediss://{username}:{password}@{host}:{port}/{database}".format(
+            username=self.redis_username,
+            password=self.redis_password,
+            host=self.redis_host,
+            port=self.redis_port,
+            database=self.redis_database,
         )
